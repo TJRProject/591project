64158ce078764503f0a6d3a953558dcc036f944c
jkravets1 <61854432+jkravets1@users.noreply.github.com>
2020-03-30 22:57:20 -0400

    Add files via upload

--- StockAnalysis/src/DataPull.java
+++ StockAnalysis/src/DataPull.java
@@ -1,18 +1,20 @@
 import java.io.BufferedReader;
+
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
+
 import javax.net.ssl.HttpsURLConnection;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+public class DataPull  {
 
-public class DataPull {
-
-	public DataPull() {
+	public DataPull () {
 		// TODO Auto-generated constructor stub
 	}
 	
@@ -50,27 +52,45 @@ public class DataPull {
         }
         return null;
     }
-
-	public static void main(String[] args) {
-		
-		
-		//api key =JRVCT84VUG4TM97S
-		//add json jar to project build path with link below
+	
+	public static void getcsv(String symbol) {
+		///api key =JRVCT84VUG4TM97S
+		//add json to project build path with link below
 		//https://www.wikihow.com/Add-JARs-to-Project-Build-Paths-in-Eclipse-(Java)
 		         try {
-		        	String json = getJSON("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&outputsize=full&apikey=JRVCT84VUG4TM97S.");
-		     		JSONObject obj;
-					obj = new JSONObject(json);
+		        	
+					String url=("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+symbol+"&outputsize=full&apikey=JRVCT84VUG4TM97S&datatype=csv");
 					
+					InputStream input = new URL(url).openStream();
+
+					FileWriter fw= new FileWriter ((symbol+".csv"), true);
+					PrintWriter pw= new PrintWriter (fw);
 					
-					System.out.println(json);
-					
-				} catch (JSONException e) {
+					 try (	InputStreamReader reader = new InputStreamReader(input, "UTF-8");
+
+			                 BufferedReader br = new BufferedReader(reader)) {
+
+			                 String line;
+
+			             while ((line = br.readLine()) != null) {
+			                 System.out.println(line);
+			                 pw.println (line);
+							 pw.flush();  
+			             }
+			         }
+
+				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-		      
+	}
 
+	public static void main(String[] args) {
+		
+		//add error catching
+		getcsv("SPY");
+		      
+		        
 	
 
 }
